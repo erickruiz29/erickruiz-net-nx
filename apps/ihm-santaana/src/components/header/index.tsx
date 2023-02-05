6203c1bf62ca1950c514fbf01f77cad7a54c4dc1
Erick Ruiz <erickruiz29@gmail.com>
2023-02-04 18:41:47 -0800

    more changes

--- apps/ihm-santaana/src/components/header/index.tsx
+++ apps/ihm-santaana/src/components/header/index.tsx
@@ -1,15 +1,24 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from 'react';
 
-import { Logo } from "@components";
+import { Logo } from '@components';
 
 export const Header: React.FC = () => {
   return (
     <div className="fluid header">
       <Logo />
+      <div className="header-menu">
+        <div>ABOUT US</div>
+        <div>EVENTS</div>
+        <div>SACRAMENTS</div>
+        <div>FAITH FORMATION</div>
+        <div>PASTORAL MINISTRIES</div>
+        <div>SUPPORT PROGRAMS</div>
+      </div>
       <div
         className="mobile-menu"
         onClick={() => {
-          const showMobileMenu = document.body.dataset.showMobileMenu === "true"
+          const showMobileMenu =
+            document.body.dataset.showMobileMenu === 'true';
           document.body.dataset.showMobileMenu = (!showMobileMenu).toString();
         }}
       >
--- apps/ihm-santaana/src/components/main/index.tsx
+++ apps/ihm-santaana/src/components/main/index.tsx
@@ -5,13 +5,13 @@ export const Main: React.FC = () => {
   return (
     <Container className="vh-100-header d-flex flex-column" fluid>
       <Row className="h-100">
-        <Col xl={{span: 2, order: 1}} md={3} className={"content-gutter left-gutter menu-list"}>
+        <Col xl={{span: 2, order: 1}} lg={3} className={"content-gutter left-gutter menu-list"}>
           Left gutter and mobile menu
         </Col>
         <Col xl={{span: 2, order: 3}} className={"content-gutter right-gutter d-none d-xl-block"}>
           Right sidebar on xl
         </Col>
-        <Col xl={{span: 8, order: 2}}  md={9} className={"main-content"}>
+        <Col xl={{span: 8, order: 2}}  md={12} lg={9} className={"main-content"}>
           Main section!!
         </Col>
       </Row>

b155b2a26669581a476622f20f5ea10498e8ccb6
Erick Ruiz <erickruiz29@gmail.com>
2023-02-15 18:28:05 -0800

    work

--- apps/erickruiz-net-nx/src/components/main/index.tsx
+++ apps/erickruiz-net-nx/src/components/main/index.tsx
@@ -3,15 +3,37 @@ import { Col, Container, Row } from "react-bootstrap";
 
 export const Main: React.FC = () => {
   return (
-    <Container className="vh-100-header d-flex flex-column" fluid>
-      <Row className="h-100">
+    <Container className="d-flex flex-column" fluid>
+      <Row className="vh-100-header">
         <Col xl={{span: 2, order: 1}} lg={3} className={"content-gutter left-gutter menu-list"}>
           Left gutter and mobile menu
         </Col>
         <Col xl={{span: 2, order: 3}} className={"content-gutter right-gutter d-none d-xl-block"}>
           Right sidebar on xl
         </Col>
-        <Col xl={{span: 8, order: 2}}  md={12} lg={9} className={"main-content"}>
+        <Col xl={{span: 8, order: 2}}  md={12} lg={9} className={"main-content primary"}>
+          Main section!!
+        </Col>
+      </Row>
+      <Row className="vh-100-header">
+        <Col xl={{span: 2, order: 1}} lg={3} className={"content-gutter left-gutter menu-list"}>
+          Left gutter and mobile menu
+        </Col>
+        <Col xl={{span: 2, order: 3}} className={"content-gutter right-gutter d-none d-xl-block"}>
+          Right sidebar on xl
+        </Col>
+        <Col xl={{span: 8, order: 2}}  md={12} lg={9} className={"main-content secondary"}>
+          Main section!!
+        </Col>
+      </Row>
+      <Row className="vh-100-header">
+        <Col xl={{span: 2, order: 1}} lg={3} className={"content-gutter left-gutter menu-list"}>
+          Left gutter and mobile menu
+        </Col>
+        <Col xl={{span: 2, order: 3}} className={"content-gutter right-gutter d-none d-xl-block"}>
+          Right sidebar on xl
+        </Col>
+        <Col xl={{span: 8, order: 2}}  md={12} lg={9} className={"main-content tertiary"}>
           Main section!!
         </Col>
       </Row>
